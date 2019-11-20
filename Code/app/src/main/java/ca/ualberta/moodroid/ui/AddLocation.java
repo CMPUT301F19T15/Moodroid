f52ffd03c4afab7cceb3acfd83c62ab83f804fc6
ubryce <bknguyen@ualberta.ca>
2019-11-19 21:23:58 -0700

    adding cameraview updates

--- Code/app/src/main/java/ca/ualberta/moodroid/ui/AddLocation.java
+++ Code/app/src/main/java/ca/ualberta/moodroid/ui/AddLocation.java
@@ -1,10 +1,14 @@
 package ca.ualberta.moodroid.ui;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.SearchView;
@@ -13,6 +17,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,6 +37,11 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
     GoogleMap mMap;
     SupportMapFragment mapFragment;
     SearchView searchView;
+
+    /**
+     * best provider
+     */
+    String bestProvider;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -114,5 +124,54 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
                 != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+
+        // set user location true to show on the map
+        mMap.setMyLocationEnabled(true);
+
+        // disable the button on the top right that takes you to your location
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        // create a new location manager to also get the Lat and Long of your location
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+        if( locationManager != null){
+            // find the location and save it
+            Location location = locationManager.getLastKnownLocation(bestProvider);
+            if( location != null){
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                // create a new LatLng variable
+                LatLng latLng = new LatLng(latitude,longitude);
+
+                // set the new LatLng variable to the camera view
+                setCameraView(latLng);
+            }
+        }
     }
+
+    /**
+     * this is where we set the initial camera view
+     *
+     * TO DO
+     * - make the initial camera view the current location of user
+     */
+    private void setCameraView(LatLng latLng){
+
+        // Set a boundary to start
+        CameraUpdate center=
+                CameraUpdateFactory.newLatLng(latLng);
+
+        // changed the zoom of the camera
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(4);
+
+        // cant do both at once so we have to move camera first
+        // then change the camera zoom
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+    }
+
 }
